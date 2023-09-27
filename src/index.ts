08283bda2247a05f9c14833ef645651001309ee2
Eugene Cross <eugene.cross@gmail.com>
2023-09-27 11:44:24 +0100

    switch to os.tmpDir() for working folder as CF will access that > refactor readfile to use fse

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import sharp from "sharp";
 import fspromises from "fs/promises";
 import fs from "fs";
+import fse from "fs-extra";
+import os from "node:os";
+import path from "path";
+
 const fetch = require("node-fetch");
 const JSZip = require("jszip");
 
@@ -24,6 +28,11 @@ const combineImageAndZip = async () => {
       { imageBuffer, annotationBuffer },
     ];
     const zip = new JSZip();
+    // Temp dir on Mac is /private/var/folders/jf/14xqzj7s6z12x5ydj0mzs71m0000gp/T
+    const workingDir = path.join(os.tmpdir(), "csvimageexport/");
+
+    await fse.ensureDir(workingDir);
+
     const filesPromise = imageAnnBufArr.map(async (image, idx) => {
       const output = await sharp(image.imageBuffer, { animated: true })
         .composite([
@@ -33,30 +42,33 @@ const combineImageAndZip = async () => {
           },
         ])
         .toBuffer();
-
-      await fspromises.writeFile(`images/output/output${idx}.jpg`, output);
+      const fileLocation = path.join(workingDir, `output${idx}.jpg`);
+      console.log(fileLocation);
+      await fspromises.writeFile(fileLocation, output);
     });
 
     // Resolve both promises so files are written
     await Promise.all(filesPromise);
 
-
     // Grab outputted files and zip into it's own folder
-    const images = await fspromises.readdir("images/output");
-    const img = zip.folder("images");
+    const images = await fspromises.readdir(workingDir);
+    const img = zip.folder("processed_images");
 
     for (const image of images) {
       if (image.endsWith(".jpg")) {
-        const imageData = await fspromises.readFile(`images/output/${image}`);
+        const imageData = await fse.readFile(path.join(workingDir, `${image}`));
         img.file(image, imageData);
       }
     }
 
     zip
-      .generateNodeStream({ type: "nodebuffer", streamfiles: true })
-      .pipe(fs.createWriteStream("images/output/output.zip"))
-      .on("finish", () => {
+      .generateNodeStream({ type: "nodebuffer", streamFiles: true })
+      .pipe(fs.createWriteStream(path.join(workingDir, "output.zip")))
+      .on("finish", async () => {
         console.log("output.zip written");
+        const finalZip = await fse.readFile(`${workingDir}output.zip`);
+        const finalZipB64 = finalZip.toString("base64");
+        return finalZipB64;
       });
   } catch (err) {
     console.error(err)
