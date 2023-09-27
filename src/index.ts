8cbcd46874a2c3719af9f2abba15a5c785f50817
Eugene Cross <eugene.cross@gmail.com>
2023-09-27 14:18:07 +0100

    add test CSV data amd add to zip

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ import path from "path";
 
 const fetch = require("node-fetch");
 const JSZip = require("jszip");
-
+import { csvData } from "./csvdata";
 const combineImageAndZip = async () => {
   const imageUrl =
     "https://firebasestorage.googleapis.com/v0/b/checklist-inspector-pro-dev.appspot.com/o/workspaces%2FhEoScaDVGCxD3V9rQFXq%2Finspections%2FlOkQi7u5pEYAlzo6bX6U%2Fphotos%2Ffor-eugene.jpg?alt=media&token=9e1d5c81-eb09-4f97-a711-53205e57a9f5";
 
   const annotationUrl =
     "https://firebasestorage.googleapis.com/v0/b/checklist-inspector-pro-dev.appspot.com/o/workspaces%2FhEoScaDVGCxD3V9rQFXq%2Finspections%2FlOkQi7u5pEYAlzo6bX6U%2Fphotos%2Ffor-eugene-annotation.png?alt=media&token=59e77878-d799-462b-a445-01852c34f1de";
-
+  
   try {
     const imageRes = await fetch(imageUrl);
     const annotationRes = await fetch(annotationUrl);
@@ -53,6 +53,8 @@ const combineImageAndZip = async () => {
     // Grab outputted files and zip into it's own folder
     const images = await fspromises.readdir(workingDir);
     const img = zip.folder("processed_images");
+
+    const csv = zip.folder("csv").file("inspection_title.csv", csvData)
 
     for (const image of images) {
       if (image.endsWith(".jpg")) {
